95ef4ebb6759713742f31ed65ef4a0d41d086ca4
roddd <chavezrodrigo526@gmail.com>
2023-05-02 01:23:23 -0600

    Inicio de la programación grupal del proyecto

---
+++ src/main/java/com/mycompany/usuariod/Usuariod.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.usuariod;
+
+/**
+ *
+ * @author chave
+ */
+public class Usuariod {
+
+    public static void main(String[] args) {
+        System.out.println("Hello amigossss!");
+    }
+}
